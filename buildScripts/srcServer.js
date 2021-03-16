35c8f6e6784a0452b548ee9d8f1d773366a7a5fd
Emmanuel <eec.studies@gmail.com>
2021-03-16 23:33:53 +0100

    set up a apis and mock data libraries that allow us to fetch data from opensource apis

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,13 @@ app.get('/',(req, res)=>{
   res.sendFile(join(__dirname, '../src/index.html'))
 })
 
+app.get('/users', (req, res)=>{
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName":"Salis", "email": "bobsalis@gmail.com"},
+    {"id": 2, "firstName": "Jane","lastName":"Gloris", "email": "jsgloris@gmail.com"},
+    {"id": 3, "firstName": "Eve", "lastName":"Benzika", "email": "eveben@gmail.com"},
+  ])
+})
 
 app.listen(port, (err)=>{
   if(err){
